1f37e6a4d7a9fdbd6ac138ee0d8d8eb05f510323
ysseo <ysseo@daerijubu.com>
2022-06-09 10:49:34 +0900

    Initial commit

---
+++ src/main/java/com/kt/ionizer/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.kt.ionizer.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/semantic/**")
+                .addResourceLocations("classpath:/semantic/")
+                .setCachePeriod(20);
+
+    }
+}
