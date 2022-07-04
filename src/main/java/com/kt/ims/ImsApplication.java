df82de5b521e32d882b39e7815fd9e101c3e0099
ysseo <ysseo3927@gmail.com>
2022-07-04 14:20:14 +0900

    테스트

--- src/main/java/com/kt/ims/ImsApplication.java
+++ src/main/java/com/kt/ims/ImsApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class ImsApplication {
 
 	public static void main(String[] args) {
+		//
 		SpringApplication.run(ImsApplication.class, args);
 	}
 
